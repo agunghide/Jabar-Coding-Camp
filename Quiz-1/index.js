b74363dcc30006b65f910f315eb8f433401c87bf
Agung Hidayat <agunghidayat10@gmail.com>
2021-09-04 13:26:10 +0700

    Function Penghasil Tanggal Hari Esok

---
+++ Quiz-1/index.js
@@ -0,0 +1,97 @@
+//Soal 1 (1.Judul : Function Penghasil Tanggal Hari Esok)
+function next_date(tanggal, bulan, tahun){
+    var bulanStr
+    
+
+    if(bulan % 2 == 1){ //Cek bulan Ganjil
+        if(tanggal >= 31){ //Cek Tanggal 31
+            tanggal = 1
+            if(bulan >= 12){
+                bulan = 1
+                tahun++
+            }else{
+                bulan++
+            }
+        }else{
+            tanggal++
+        }
+    }else{
+        if(tahun % 4 == 0){ // Cek Tahun Kabisat
+            if(bulan==2){
+                if(tanggal >= 29){ //Cek tanggal 29
+                    tanggal = 1
+                    if(bulan >= 12){
+                        bulan = 1
+                        tahun++
+                    }else{
+                        bulan++
+                    }
+                }else{
+                    tanggal++
+                }
+            }else{
+                if(tanggal >= 30){ //Cek tanggal 29
+                    tanggal = 1
+                    if(bulan >= 12){
+                        bulan = 1
+                        tahun++
+                    }else{
+                        bulan++
+                    }
+                }else{
+                    tanggal++
+                }
+            }
+        } else{
+            if(bulan==2){
+                if(tanggal >= 28){ //Cek tanggal 28
+                    tanggal = 1
+                    if(bulan >= 12){
+                        bulan = 1
+                        tahun++
+                    }else{
+                        bulan++
+                    }
+                }else{
+                    tanggal++
+                }
+            }else{
+                if(tanggal >= 30){ //Cek tanggal 29
+                    tanggal = 1
+                    if(bulan >= 12){
+                        bulan = 1
+                        tahun++
+                    }else{
+                        bulan++
+                    }
+                }else{
+                    tanggal++
+                }
+            }
+        }
+    }
+
+    switch(bulan){ //Ubah Bulan Menjadi String
+        case 1: { bulanStr = "Januari";break;}
+        case 2: { bulanStr = "Februari";break;}
+        case 3: { bulanStr = "Maret";break;}
+        case 4: { bulanStr = "April";break;}
+        case 5: { bulanStr = "Mei";break;}
+        case 6: { bulanStr = "Juni";break;}
+        case 7: { bulanStr = "Juli";break;}
+        case 8: { bulanStr = "Agustus";break;}
+        case 9: { bulanStr = "September";break;}
+        case 10: { bulanStr = "Oktober";break;}
+        case 11: { bulanStr = "November";break;}
+        case 12: { bulanStr = "Desember";break;}
+        default:{bulanStr = "Bulan Error"}
+    }
+
+    console.log(tanggal + " " + bulanStr + " " + tahun)
+}
+
+var tanggal = 29
+var bulan = 2
+var tahun = 2020
+
+next_date(tanggal , bulan , tahun)
